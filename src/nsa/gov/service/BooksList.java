f9b55829e8e819b61b3ce9acb876be6145754719
Lucas Churchill <lucas@agtlucas.com>
2014-08-11 07:36:40 -0300

    Update BookList

--- src/nsa/gov/service/BooksList.java
+++ src/nsa/gov/service/BooksList.java
@@ -1,5 +1,24 @@
 package nsa.gov.service;
 
+import java.util.List;
+
+import javax.jws.WebService;
+import javax.xml.ws.Endpoint;
+
+import nsa.gov.dao.BookDAO;
+import nsa.gov.model.Book;
+
+@WebService
 public class BooksList {
+	
+	public List<Book> listBooks() {
+		BookDAO dao = new BookDAO();
+		return dao.getBooks();
+	}
+	
+	public static void main(String[] args) {
+		Endpoint.publish("http://localhost:8180/books", new BooksList());
+		System.out.println("Go!");
+	}
 
 }
