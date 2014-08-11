34bad7447d49cdc11b4624b6267b79438afcdab3
Lucas Churchill <lucas@agtlucas.com>
2014-08-11 07:27:55 -0300

    Update Book model

--- src/nsa/gov/model/Book.java
+++ src/nsa/gov/model/Book.java
@@ -1,5 +1,67 @@
 package nsa.gov.model;
 
+import java.util.List;
+
 public class Book {
+	
+	private String name;
+	private String publisher;
+	private String summary;
+	private int year;
+	private List<String> authors;
+	
+	public Book() {
+		super();
+	}
+	
+	public Book(String name, String publisher, String summary, int year, List<String> authors) {
+		super();
+		this.name = name;
+		this.publisher = publisher;
+		this.summary = summary;
+		this.year = year;
+		this.authors = authors;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public List<String> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(List<String> authors) {
+		this.authors = authors;
+	}
+	
 
 }
