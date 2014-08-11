56adc57011a19f2527e839f3689bfa349dc55f47
Lucas Churchill <lucas@agtlucas.com>
2014-08-11 07:32:01 -0300

    Update BookDAO

--- src/nsa/gov/dao/BookDAO.java
+++ src/nsa/gov/dao/BookDAO.java
@@ -4,6 +4,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import nsa.gov.model.Book;
+
 public class BookDAO {
+	
+	public List<Book> getBooks() {
+		List<Book> books = new ArrayList<Book>();
+		
+		Book book1 = new Book();
+		book1.setName("Iron Maiden 666");
+		book1.setPublisher("EMI");
+		book1.setSummary("Scream for me 2015");
+		book1.setYear(2003);
+		book1.setAuthors(Arrays.asList("Steve Harris, Bruce Dickinson"));
+		books.add(book1);
+		
+		return books;
+	}
 
 }
